25c480fdf599081756c8afc0bb5f6a63395da0bd
s0a1e2 <oops2198@gmail.com>
2023-10-26 13:09:44 +0330

    update user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,31 @@
-export class User {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class UserEntity {
+  @PrimaryGeneratedColumn({ type: 'int2' })
+  id: number;
+
+  @Column({ unique: true, type: 'varchar' })
+  username: string;
+
+  @Column({ unique: true, type: 'varchar' })
+  email: string;
+
+  @Column({ type: 'varchar' })
+  shippingAddress: string;
+
+  @Column({ type: 'varchar' })
+  role: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+}
